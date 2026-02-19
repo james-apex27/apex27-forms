62eb818617379dc8f5276572795f8df7aa94028e
James Wright <james@apex27.co.uk>
2026-02-19 22:05:18 +0000

    updated index

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-header('Location: https://apex27.co.uk');
-exit;
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Apex27 Form Hosting</title>
+</head>
+<body>
+	<h1>This is the Apex27 form hosting server, there's nothing to see here.</h1>
+	<a href="https://apex27.co.uk">Go to Apex27</a>
+</body>
+</html>
